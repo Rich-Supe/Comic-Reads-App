704202019fbeba5e839a938ec1ce1464ef3cf852
Rich-Supe <rsupe1993@aol.com>
2021-05-03 17:46:18 -0400

    added user login auth

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const logger = require('morgan');
 const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/user');
+const { sessionSecret } = require('./config');
+const { restoreUser } = require('./auth');
+
 
 const app = express();
 
@@ -17,15 +21,17 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(sessionSecret));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(loginUser);
 
 // set up session middleware
 const store = new SequelizeStore({ db: sequelize });
 
 app.use(
   session({
-    secret: 'superSecret',
+    name: 'comic-list.sid',
+    secret: sessionSecret,
     store,
     saveUninitialized: false,
     resave: false,
@@ -34,9 +40,9 @@ app.use(
 
 // create Session table if it doesn't already exist
 store.sync();
-
+app.use(restoreUser);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ db/migrations/20210503174927-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      emailAddress: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+        unique: true
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
