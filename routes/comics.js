54716bacdbbe288e7ed06b96473cbbfcf4981ffa
stephenchoung <stephen.choung1@gmail.com>
2021-05-04 10:45:16 -0400

    stephen added basic patch and button javascript

--- routes/comics.js
+++ routes/comics.js
@@ -5,8 +5,7 @@ const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 const { requireAuth } = require('../auth');
 const { Comics } = db;
-const { check, validationResult } = require('express-validator'); 
-const { requireAuth } = require("../auth");
+const { check, validationResult } = require('express-validator');
 
 router.use(requireAuth)
 
@@ -14,15 +13,45 @@ router.use(requireAuth)
 
 router.get('/', asyncHandler(async(req, res) => {
     const comics = await Comics.findAll();
-  
+
     res.render("comics", { comics })
 }));
 
 router.get('./:id(\\d+)', asyncHandler(async(req, res) => {
-    
+
     res.render('(:')
 }));
 
+//Stephen - Updating Database///////////////////////////////////////////////////////////////////////////////////////////
+// Stephen - I want to find user and then patch "wants to read"
+router.patch('./:id(\\d+)', asyncHandler(async(req, res) => {
+    const comicId = parseInt(req.params.id, 10);
+    //gotta find model root for the "want to read"
+    const wantToRead = await user.findByPk(comicId, {
+        includes: 'JoinComicToUser'
+    });
+    console.log(wantToRead)
+    if(wantToRead === false){
+        await wantToRead.update(true);
+    } else {
+        await wantToRead.update(false);
+    }
+}));
 
-
+// Stephen - I want to find user and then patch "has read"
+router.patch('./:id(\\d+)', asyncHandler(async(req, res) => {
+    const comicId = parseInt(req.params.id, 10);
+    //gotta find model root for the "want to read"
+    const hasRead = await user.findByPk(comicId, {
+        includes: 'JoinComicToUser'
+    });
+    console.log(hasRead)
+    //figure out what is in the body and updated the has_read
+    if(hasRead === false){
+        await hasRead.update(true);
+    } else {
+        await hasRead.update(false);
+    }
+}));
+//End of Stephen//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = router
