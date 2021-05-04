ef4dd5e13627a8fa6280e7e4faac00f08cd5fb58
Nathan Mount <nathanmount10@gmail.com>
2021-05-04 10:32:43 -0700

    restored database config to normal

--- config/database.js
+++ config/database.js
@@ -3,22 +3,22 @@ const {
 } = require('./index');
 
 module.exports = {
-  "development": {
-    "username": "app_academy",
-    "password": "password",
-    "database": "comic_reads",
-    "host": "127.0.0.1",
-    "dialect": 'postgres',
-    "seederStorage": 'sequelize',
+development: {
+  username,
+  password,
+  database,
+  host,
+  dialect: 'postgres',
+  seederStorage: 'sequelize',
   },
 };
 
-// development: {
-//   username,
-//   password,
-//   database,
-//   host,
-//   dialect: 'postgres',
-//   seederStorage: 'sequelize',
+// "development": {
+//   "username": "app_academy",
+//   "password": "password",
+//   "database": "comic_reads",
+//   "host": "127.0.0.1",
+//   "dialect": 'postgres',
+//   "seederStorage": 'sequelize',
 // },
 // };
