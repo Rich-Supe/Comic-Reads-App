292b7d782f3c5bd736aef280eaaa9054b8f25967
Rich-Supe <rsupe1993@aol.com>
2021-05-05 19:54:45 -0400

    fixed shelf bug

--- public/javascripts/shelfButton.js
+++ public/javascripts/shelfButton.js
@@ -6,7 +6,7 @@ const shelfButton = document.querySelector(".shelf_btn");
 shelfButton.addEventListener("click", async (e) => {
     // const currentComicId = comic.id
     // if (!)
-    const userId = ;
+    // const userId = ;
     const shelfButtonId = event.target.id
     console.log('This comic was added to your shelf!');
     // await console.log(comic)
