8f0ad107b7e0592f6618425f2a5f6bc8c19a15b1
Nathan Mount <nathanmount10@gmail.com>
2021-05-05 11:10:21 -0700

    added incomplete code to reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,31 +2,80 @@ const express = require('express');
 const { asyncHandler } = require('./utils');
 const db = require('../db/models');
 const { requireAuth } = require('../auth');
-const { Review } = db;
-const { check, validationResult } = require('express-validator');
-
+const { Review, User } = db;
+// const { check } = require('express-validator');
 const router = express.Router();
-
 router.use(requireAuth);
 
+const { token } = req
+const { id } = jwt.decode(token).data
+const comicId = parseInt(req.params.comicId, 10)
 
+// view reviews
+router.get( '/comics/:id(\\d+)', asyncHandler(async (req, res) => {
+    const reviews = await Review.findAll({
+        where: {
+            comicId: comicId
+        },
+    });
+    res.json({ reviews });
+    })
+);
 
-
-router.post('/comics/:comicId(\\d+)', asyncHandler( async(req, res) => {
-    const { token } = req
-    const { id } = jwt.decode(token).data
-
-    const comicId = parseInt(req.params.comicId, 10)
+// post new comment
+router.post('/comics/:id(\\d+)', asyncHandler( async(req, res) => {
     const review = req.body.review
     const comment = await Review.create({ review, userId: id, comicId: comicId });
-
     const comments = await Review.findAll({
         where: {
             comicId: comicId
         }
-    })
-
+    });
     res.status(201).json({ comment, comments })
-}))
+}));
+
+// edit review comment
+router.put('/comics/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const review = await Review.findOne({
+        where: {
+            comicId: comicId
+        },
+    });
+    if (review) {
+     await review.update({ review: req.body.review });
+     res.json({ review });
+    }
+}));
+
+
+// delete thingamajiggy(comment hopefully)
+router.delete('/comics/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    const review = await Review.findOne({
+        where: {
+            comicId: comicId
+        }
+    });
+    if (tweet) {
+        await review.destroy();
+        res.json({ review: `Deleted comment with id of ${req.params.id}`});
+    }
+}));
+
+
+// add rating
+router.put('/comics/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const rating = await Comic.findByPk({
+        where: {
+            comicId: comicId
+        },
+        include: [Review.rating],
+    });
+    if (review) {
+     await review.update({ review: req.body.review });
+     res.json({ review });
+    }
+}));
+
+
 
 module.exports = router;
