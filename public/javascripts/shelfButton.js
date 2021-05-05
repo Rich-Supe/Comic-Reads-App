4e7a23afb47b4aa8b0b4711966faebbd72433763
Rich-Supe <rsupe1993@aol.com>
2021-05-05 18:44:19 -0400

    added button pug

---
+++ public/javascripts/shelfButton.js
@@ -0,0 +1,12 @@
+
+
+const shelfButton = document.querySelector(".shelf_btn");
+
+shelfButton.addEventListener("click", async (e) => {
+    // e.preventDefault()
+    // const currentComicId = comic.id
+    // // if (!)
+    // console.log('This comic was added to your shelf!');
+
+
+})
