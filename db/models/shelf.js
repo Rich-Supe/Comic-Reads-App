97d7f6da336dffc41b338c48c4b94180a9ebe67d
Rich-Supe <rsupe1993@aol.com>
2021-05-06 17:12:29 -0400

    solved libraries created at errors

--- db/models/shelf.js
+++ db/models/shelf.js
@@ -3,18 +3,17 @@ module.exports = (sequelize, DataTypes) => {
   const Shelf = sequelize.define('Shelf', {
     name: DataTypes.STRING,
     isRecommended: DataTypes.BOOLEAN,
-    comicId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER
   }, { timestamps: false });
   Shelf.associate = function(models) {
     // associations can be defined here
     // Shelf.hasMany(models.Comic, { foreignKey: 'shelfId'});
-    const columnMapping2 = {
+    const columnMapping = {
       through: 'Libraries',
       foreignKey: 'shelfId',
       otherKey: 'comicId'
     }
-    Shelf.belongsToMany( models.Comic, columnMapping2);
+    Shelf.belongsToMany( models.Comic, columnMapping);
 
     Shelf.belongsTo(models.User, { foreignKey: 'userId'})
   };
