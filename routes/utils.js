e4a0749e0a9da016db839b603d16a1a0d20208d2
Rich-Supe <rsupe1993@aol.com>
2021-05-07 14:16:28 -0400

    shelf work

--- routes/utils.js
+++ routes/utils.js
@@ -8,3 +8,18 @@ module.exports = {
   csrfProtection,
   asyncHandler,
 };
+
+
+// let ans = await res.json()
+
+//       if(ans.post === "exists"){
+//         const res = await fetch("http://localhost:8080/comics", {
+//           method: 'PATCH',
+//           body: JSON.stringify(body),
+//           headers: {"Content-Type": "application/json",}
+//         });
+//       }
+//     } catch (e){
+//       console.log(e);
+//     }
+//   })
