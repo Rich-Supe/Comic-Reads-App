5d0358f6efb9e9b774d141091118997201aa1143
Nathan Mount <nathanmount10@gmail.com>
2021-05-07 14:17:34 -0700

    need review

--- public/javascripts/reviewForm.js
+++ public/javascripts/reviewForm.js
@@ -1,21 +1,45 @@
-const submitBtn = document.querySelector(".submitReviewBtn");
-const commentValue = document.querySelector('.comment').value;
-submitBtn.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const body = { commentValue };
-  try {
-    const res = await fetch("/comics/:id(\\d+)/review", {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    // console.log(res);
-    if (!res.ok) {
-      throw res;
+// const submitBtn = document.querySelector(".submitReviewBtn");
+// const commentValue = document.querySelector('.comment').value;
+// submitBtn.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const body = { commentValue };
+//   try {
+//     const res = await fetch("/comics/:id(\\d+)/review", {
+//       method: "POST",
+//       body: JSON.stringify(body),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     // console.log(res);
+//     console.log('bro')
+//     if (!res.ok) {
+//       throw res;
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+const editBtn = document.querySelectorAll('.editBtn');
+
+editBtn.forEach(btn => {
+  btn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const url = window.location.href;
+    const comicString = 'comics';
+    let cIndex;
+    for (let i = 0; i < url.length; i++) {
+      let j = i + 6;
+      let urlString = url.slice(i, j);
+      if (urlString === comicString) {
+        cIndex = j;
+        break;
+      }
     }
-  } catch (err) {
-    console.log(err);
-  }
+    const comicId = url.slice(cIndex, 1);
+    console.log(url);
+    console.log(comicId, '================================================');
+    window.location.href = `./${comicId}/edit/${btn.id}/`;
+  })
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,92 +7,38 @@ const { check } = require('express-validator');
 const router = express.Router();
 router.use(requireAuth);
 
-// const handleValidationErrors = (req, res, next) => {
-//     const validationErrors = validationResult(req);
 
-//     if (!validationErrors.isEmpty()) {
-//       const errors = validationErrors.array().map((error) => error.msg);
+//post review
+router.post(
+    "/:id(\\d+)/review",
+    asyncHandler(async (req, res) => {
+        const { commentArea } = req.body;
+        await Review.create({ review: commentArea, userId: res.locals.user.id, comicId: req.params.id});
+        res.redirect(`/comics/${req.params.id}`);
+    })
+);
 
-//       const err = Error("Bad request.");
-//       err.status = 400;
-//       err.title = "Bad request.";
-//       err.errors = errors;
-//       return next(err);
-//     }
-//     next();
-// };
+// router.get("/:id(\\d+)/review/:id(\\d+)", asyncHandler(async(req, res) => {
+//     const reviewId = parseInt(req.params.id, 10)
+//     const review = await db.Review.findByPk(reviewId);
+// }))
 
-// const reviewNotFoundError = (id) => {
-// const err = Error("Review not found");
-// err.errors = [`Review with id of ${id} could not be found.`];
-// err.title = "Review not found.";
-// err.status = 404;
-// return err;
-// };
 
-// const validateReview = [
-// check("review")
-//     .exists({ checkFalsy: true })
-//     .withMessage("Review can't be empty."),
-// //  message cannot be longer than 280 characters:
-// check("review")
-//     .isLength({ max: 280 })
-//     .withMessage("Review can't be longer than 280 characters."),
-// handleValidationErrors,
-// ];
+//edit review
+router.post(
+    "/:id(\\d+)/edit/",
+    asyncHandler(async (req, res) => {
+        let comicId = Comic.findOne({where:{comicId}})
+        const targetReview = Review.findByPk(editBtn.id);
+        const { commentArea } = req.body;
+        await targetReview.update({ review: commentArea, userId: res.locals.user.id, comicId: req.params.id });
+        res.render(`review-edit.pug`);
+    })
+);
 
-// router.get(
-// "/comics/:id(\\d+)",
-// asyncHandler(async (req, res, next) => {
-//     const review = await Review.findOne({
-//     where: {
-//         id: req.params.id,
-//     },
-//     });
-//     if (review) {
-//     res.json({ review });
-//     } else {
-//     next(reviewNotFoundError(req.params.id));
-//     }
-// })
-// );
 
-// router.post(
-//   "/:id(\\d+)/review",
-//   asyncHandler(async (req, res) => {
-//       const { commentArea } = req.body;
-//       await Review.create({ review: commentArea, userId: res.locals.user.id, comicId: req.params.id });
-//       res.redirect(`/comics/${req.params.id}`);
-//   })
-// );
-
-// router.put(
-// "/comics/:id(\\d+)",
-// validateReview,
-// asyncHandler(async (req, res, next) => {
-//     const review = await Review.findOne({
-//     where: {
-//         id: req.params.id,
-//     },
-//     });
-//     if (req.user.id !== review.userId) {
-//     const err = new Error("Unauthorized");
-//     err.status = 401;
-//     err.message = "You are not authorized to edit this review.";
-//     err.title = "Unauthorized";
-//     throw err;
-//     }
-//     if (review) {
-//     await review.update({ review: req.body.review });
-//     res.json({ review });
-//     } else {
-//     next(reviewNotFoundError(req.params.id));
-//     }
-// })
-// );
-
-// router.delete(
-//     "/comics/:id(\\d+)",
+//   router.put(
+//     "/:id(\\d+)/edit/:id(\\d+)",
 //     asyncHandler(async (req, res, next) => {
 //       const review = await Review.findOne({
 //         where: {
@@ -102,48 +48,46 @@ router.use(requireAuth);
 //       if (req.user.id !== review.userId) {
 //         const err = new Error("Unauthorized");
 //         err.status = 401;
-//         err.message = "You are not authorized to delete this review.";
+//         err.message = "You are not authorized to edit this review.";
 //         err.title = "Unauthorized";
 //         throw err;
 //       }
 //       if (review) {
-//         await review.destroy();
-//         res.json({ message: `Deleted review with id of ${req.params.id}.` });
-//       } else {
-//         next(reviewNotFoundError(req.params.id));
+//         await review.update({ review: req.body.review });
+//         res.redirect(`/comics/${req.params.id}`);
 //       }
+//     // else {
+//     //     next(tweetNotFoundError(req.params.id));
+//     //   }
 //     })
 //   );
 
-// post new comment
+  //delete review
+  router.delete(
+    "/:id",
+    asyncHandler(async (req, res, next) => {
+      const review = await Review.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (req.user.id !== review.userId) {
+        const err = new Error("Unauthorized");
+        err.status = 401;
+        err.message = "You are not authorized to delete this review.";
+        err.title = "Unauthorized";
+        throw err;
+      }
+      if (review) {
+        await review.destroy();
+        res.redirect(`/comics/${req.params.id}`);
+      }
+    //   else {
+    //     next(tweetNotFoundError(req.params.id));
+    //   }
+    })
+  );
 
-
-// // edit review comment
-// router.put('/comics/:id(\\d+)', asyncHandler(async(req, res, next) => {
-//     const review = await Review.findOne({
-//         where: {
-//             comicId: comicId
-//         },
-//     });
-//     if (review) {
-//      await review.update({ review: req.body.review });
-//      res.json({ review });
-//     }
-// }));
-
-
-// // delete thingamajiggy(comment hopefully)
-// router.delete('/comics/:id(\\d+)', asyncHandler(async (req, res, next) => {
-//     const review = await Review.findOne({
-//         where: {
-//             comicId: comicId
-//         }
-//     });
-//     if (review) {
-//         await review.destroy();
-//         res.json({ review: `Deleted comment with id of ${req.params.id}`});
-//     }
-// }));
 
 
 module.exports = router;
