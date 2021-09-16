9e1b983df1153ab4d5818a9a7e735afb8404fefb
msineath <mikesineath@gmail.com>
2021-09-15 20:34:41 -0400

    fixed 'search by' functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,38 +2,21 @@ const searchButton = document.querySelector('.search_btn');
 let searchCriteria = document.getElementById('criteria');
 let secondDropdown = document.getElementById('secondDropdown');
 let selection = document.getElementById('searchTitle');
-let title = document.getElementById('title')
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let genre = document.getElementById('genre');
 
 title.addEventListener('change', () => {
-    console.log(`title: ${title.value}`)
-    // await fetch (`/comics/search/title/${title.value}`);
-    // window.location.href = `/comics/search/title/${title.value}`
+    window.location.href = `/comics/search/title/${title.value}`
 })
-// searchCriteria.addEventListener('change', event => {
-//     let criteria = searchCriteria.value;
-//     switch (criteria) {
-//         case 'keyword':
-            // selection = document.getElementById('searchTitle');
-            // selection.style.visibility = 'visible';
-        //     break;
-        // case 'title':
-        //     secondDropdown.style.visibility = 'visible';
-        //     selection = document.getElementById('title');
-        //     selection.style.visibility = 'visible';
-        //     break;
-        // case 'author':
-        //     secondDropdown.style.visibility = 'visible';
-        //     selection = document.getElementById('author');
-        //     selection.style.visibility = 'visible';
-        //     break;
-        // case 'genre':
-        //     secondDropdown.style.visibility = 'visible';
-        //     selection = document.getElementById('genre');
-        //     selection.style.visibility = 'visible';
-        //     break;
-    // }
-    
-// })
+
+author.addEventListener('change', () => {
+    window.location.href = `/comics/search/author/${author.value}`
+})
+
+genre.addEventListener('change', () => {
+    window.location.href = `/comics/search/genre/${genre.value}`
+})
 
 searchButton.addEventListener("click", async (event)=> {
     console.log(`searchCriteria:  selection: ${selection.value}`)
